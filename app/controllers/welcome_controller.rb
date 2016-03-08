3b237ef865d8442630aff30363961ad24b3a4248
taral-almoula <taral.almoula@gmail.com>
2016-03-08 00:13:15 -0600

    Commit 7

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,16 +4,16 @@ def index
 end
 def datasend
 	
-	render inline: [{aname: 'Jesse Eisenberg', gender: 'age', address: ' Queens,NY,USA,', age: '30'},
-					{aname: 'Amitabh Bachchan', gender: 'age', address: ' Allahbad, India', age: '70'},
-					{aname: 'Kevin Spacey', gender: 'age', address: 'South Orange,NJ,USA', age: '52'},
-					{aname: 'Scarlett Johansson', gender: 'Feage', address: ' Manhattan,NY,USA', age: '33'},
-					{aname: 'Akshay Kumar', gender: 'age', address: 'Amritsar, India', age: '45'},
-					{aname: 'Neil Patrick Harris', gender: 'age', address: ' Albuquerque, NM, USA', age: '40'},
-					{aname: 'Zooey Deschanel', gender: 'Feage', address: 'Los Angeles, CA, USA', age: '27'},
-					{aname: 'Hugh Jackman', gender: 'age', address: 'Sydney, Australia', age: '47'},
-					{aname: 'Anne Hathway', gender: 'Feage', address: 'Brooklyn, NY, USA', age: '38'},
-					{aname: 'Jim Carrey', gender: 'age', address: ' Newmarket, Canada', age: '52'}].to_json
+	render inline: [{aname: 'Jesse Eisenberg', gender: 'Male', address: ' Queens,NY,USA,', age: '30'},
+					{aname: 'Amitabh Bachchan', gender: 'Female', address: ' Allahbad, India', age: '70'},
+					{aname: 'Kevin Spacey', gender: 'Male', address: 'South Orange,NJ,USA', age: '52'},
+					{aname: 'Scarlett Johansson', gender: 'Female', address: ' Manhattan,NY,USA', age: '33'},
+					{aname: 'Akshay Kumar', gender: 'Male', address: 'Amritsar, India', age: '45'},
+					{aname: 'Neil Patrick Harris', gender: 'Male', address: ' Albuquerque, NM, USA', age: '40'},
+					{aname: 'Zooey Deschanel', gender: 'Female', address: 'Los Angeles, CA, USA', age: '27'},
+					{aname: 'Hugh Jackman', gender: 'Male', address: 'Sydney, Australia', age: '47'},
+					{aname: 'Anne Hathway', gender: 'Female', address: 'Brooklyn, NY, USA', age: '38'},
+					{aname: 'Jim Carrey', gender: 'Male', address: ' Newmarket, Canada', age: '52'}].to_json
   end
 end
 
